2f35729a03aaa65d7119be83fe50ffc93b956e18
charlottexhobiso <68104405+charlottexhobiso@users.noreply.github.com>
2020-08-04 15:53:18 +0200

    Initial
    GIS to Web

---
+++ data/FaultLines_r_7.js
@@ -0,0 +1,15 @@
+var json_FaultLines_r_7 = {
+"type": "FeatureCollection",
+"name": "FaultLines_r_7",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "id": "1", "StrName": "Haldon Weg", "PipeM": "Steel", "PipeDia": "450" }, "geometry": { "type": "MultiLineString", "coordinates": [ [ [ 26.165464392749989, -29.126896259916379 ], [ 26.193797304891937, -29.125765624102865 ], [ 26.197792898776949, -29.123507547240941 ] ] ] } },
+{ "type": "Feature", "properties": { "id": "2", "StrName": "Sand du Plessis Ave", "PipeM": "Asbes", "PipeDia": "350" }, "geometry": { "type": "MultiLineString", "coordinates": [ [ [ 26.266872675250475, -29.094259469542468 ], [ 26.272295975369094, -29.114781091863325 ] ] ] } },
+{ "type": "Feature", "properties": { "id": "3", "StrName": "King Edward", "PipeM": "Cast Iron", "PipeDia": "100" }, "geometry": { "type": "MultiLineString", "coordinates": [ [ [ 26.19951750114722, -29.119978353073972 ], [ 26.211703408963796, -29.122530539273662 ] ] ] } },
+{ "type": "Feature", "properties": { "id": "4", "StrName": "Kelner Street", "PipeM": "Cast Iron", "PipeDia": "100" }, "geometry": { "type": "MultiLineString", "coordinates": [ [ [ 26.205147383021096, -29.108607409705083 ], [ 26.219523552488685, -29.111532775576375 ] ] ] } },
+{ "type": "Feature", "properties": { "id": "5", "StrName": "Reid Street", "PipeM": "Cast Iron", "PipeDia": "100" }, "geometry": { "type": "MultiLineString", "coordinates": [ [ [ 26.206069979709127, -29.105865094778469 ], [ 26.213587799067778, -29.107516142280282 ] ] ] } },
+{ "type": "Feature", "properties": { "id": "6", "StrName": "Watkey Street", "PipeM": "Cast Iron", "PipeDia": "100" }, "geometry": { "type": "MultiLineString", "coordinates": [ [ [ 26.220375752693801, -29.126871743373961 ], [ 26.218299313471114, -29.134916685737288 ] ] ] } },
+{ "type": "Feature", "properties": { "id": "7", "StrName": "Eerstelaan", "PipeM": "Cast Iron", "PipeDia": "100" }, "geometry": { "type": "MultiLineString", "coordinates": [ [ [ 26.213839422759929, -29.107552546986902 ], [ 26.210345263273677, -29.120665469426363 ] ] ] } },
+{ "type": "Feature", "properties": { "id": "9", "StrName": "Long Street", "PipeM": "Cast Iron", "PipeDia": "100" }, "geometry": { "type": "MultiLineString", "coordinates": [ [ [ 26.22767226944487, -29.11578313431848 ], [ 26.232886161561204, -29.113577577381708 ], [ 26.239390746224938, -29.108910787071778 ], [ 26.24736004559276, -29.099641188993637 ], [ 26.252440050531504, -29.093778127597442 ] ] ] } }
+]
+}
